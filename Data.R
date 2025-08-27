2433cf370975375511d0beb47e2458653be0de92
AnaPineda <apineda@uthsc.edu>
2025-08-27 16:26:30 -0500

    Class 1 data creation

---
+++ Data.R
@@ -0,0 +1,22 @@
+#' title: "TSCI 5230 Processing a Data Set"
+#' Author: Ana Pineda
+##Copy over the init section
+debug <- 0;seed <-22;#See is to generate a random number but in a different way. You will have a random number and reproducibility.
+
+knitr::opts_chunk$set(echo=debug>-1, warning=debug>0, message=debug>0, class.output="scroll-20", attr.output='style="max-height: 150px; overflow-y: auto;"');
+
+library(ggplot2); # visualization
+library(GGally);
+library(rio);# simple command for importing and exporting
+library(pander); # format tables
+#library(printr); # set limit on number of lines printed
+library(broom); # allows to give clean dataset
+library(dplyr);#add dplyr library
+library(tidymodels);
+library(ggfortify);
+#init----
+options(max.print=500);
+panderOptions('table.split.table',Inf); panderOptions('table.split.cells',Inf)
+datasource <- "../output/csv/"
+#data0<-import(list.files(datasource,full.names = T) [9]) is to name files to identify/ specific files
+data0<-sapply(list.files(datasource,full.names = T),import) #how to create name files of many files at the same time
