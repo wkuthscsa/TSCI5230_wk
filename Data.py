0963eed19670d6a85a1d0f056548d2d931b7789e
Kelly <kellyw@win.uthscsa.edu>
2025-11-05 16:58:02 -0600

    We worked on list and dictionary comprehension as well as deconstructing and rezipping a dictionary

--- Data.py
+++ Data.py
@@ -1,6 +1,8 @@
 
 import pandas as pd
 import numpy as np
+import pprint
+
 from glob import glob
 from datetime import datetime
 
@@ -43,8 +45,9 @@ unique_patients = diabetesjunk["PATIENT"].unique()
 unique_encounters = diabetesjunk["ENCOUNTER"].unique()
 
 # Filter medications for Metformin
-medications = data0["medications"]
+medications=data0['medications'].copy(deep=True)
 met_meds = medications[medications["CODE"].isin(met_rxnorm_lookup["rxcui"])]
+medications['total_cost_rounded']=medications['TOTALCOST'].round
 
 # Filter patients and encounters related to diabetes
 patients = data0["patients"]
@@ -74,12 +77,12 @@ met_Ljoin = pd.merge(
 patients["DEATHDATE"] = pd.to_datetime(patients["DEATHDATE"], errors="coerce")
 patients["BIRTHDATE"] = pd.to_datetime(patients["BIRTHDATE"], errors="coerce")
 patients["alive"] = patients["DEATHDATE"].isna()
-#patients["age"] = ((np.minimum(datetime.today(), patients["DEATHDATE"].fillna(datetime.today())) - patients["BIRTHDATE"]).dt.days) / 365.25
+patients["age"] = (patients["DEATHDATE"].fillna(datetime.today()) - patients["BIRTHDATE"]).dt.days / 365.25
 age_summary = patients.groupby("alive")["age"].agg(
     avg_age="mean",
     min_age_at_death_or_censor="min",
-    max_age_at_death_or_censor=max",
-    count=count"
+    max_age_at_death_or_censor="max",
+    count="count"
 ).reset_index()
 
 
@@ -87,3 +90,42 @@ age_summary = patients.groupby("alive")["age"].agg(
 
 print(age_summary)
 
+#Converting column to a Python list (what R calls a vector)
+medications['TOTALCOST'].tolist()
+total_cost_list=medications['TOTALCOST'].tolist()
+total_cost_list.round()
+round(total_cost_list)
+
+#There are several ways to transform a value in a pyton list. Below you will find 3 commands to round a number in this list.
+result=[]
+for xx in total_cost_list:
+    result.append(round(xx))
+    #result=result+round(xx)
+    #result+=[round(xx)]
+
+#List Comprehension is creating and modifying a list in the same line of code
+#Another way to do this
+result2=[round(xx) for xx in total_cost_list]
+result2
+result3=[round(xx) if xx >10 else -1 for xx in total_cost_list ] # the if statement is nested in the for loop and the round function is inside the if statement
+result3
+
+#Dictionary
+dresult={}
+for xx in  data0.keys():
+    #dresult[xx]=data0[xx].keys().tolist()
+    dresult.update({xx:data0[xx].keys().tolist()}) #the ":" in this line is to signify the name:value pairs of the dataframes, like allergies:START
+pprint.pprint(dresult) #to see the dictionary list in a more formated format
+
+dresult2={xx:data0[xx].keys().tolist() for xx in data0.keys()}
+pprint.pprint(dresult)
+
+#Creating Dictionary with Zip
+#Create list with of keys  then create a with table names using list comprehension
+table_names=data0.keys()
+column_names=[data0[xx].keys().tolist()for xx in table_names]
+column_names
+dresult3=dict(zip(table_names,column_names))
+pprint.pprint(dresult3)
+
+unzi    
